f6e82844816ced49d733926befcef1303cbc373c
Ricardo Wojciechowski <ricardowoj@hotmail.com>
2020-05-24 23:43:59 -0300

    remove user in routes

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,4 @@
 const routes = require('express').Router()
 const { User } = require('./app/models')
 
-User.create({
-  name: "Ricardo",
-  email: "ricardowoj@hotmail.com",
-  password_hash: "123456789"
-
-})
-
 module.exports = routes
