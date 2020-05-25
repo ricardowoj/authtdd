8bab2bb1c8bd12b90304855c4e337c71fb4300ee
Ricardo Wojciechowski <ricardowoj@hotmail.com>
2020-05-24 23:10:40 -0300

    refactor User, change to name DataTpes

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,4 @@
-module.exports = (sequelize, DataTupes) => {
+module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define("User", {
     name: DataTypes.STRING,
     email: DataTypes.STRING,
