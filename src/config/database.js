8fb589b0bf27aca6a872616ab8f38747e94ef19e
Ricardo Wojciechowski <ricardowoj@hotmail.com>
2020-05-24 22:41:15 -0300

    add create-users in migrations

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
   host: '127.0.0.1',
-  username: 'docker',
-  password: 'docker',
+  username: 'postgres',
+  password: 'admin',
   database: 'nodeauth',
   dialect: 'postgres',
   operatorsAliases: 'postgres',
